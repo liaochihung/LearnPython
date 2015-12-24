c67d2d245fa660e93f08285298eca2ddf7022dd4
Jack <gihomn@gmail.com>
2015-12-24 13:23:27 +0800

    test socket

--- 2.data/sqlite_test.py
+++ 2.data/sqlite_test.py
@@ -4,26 +4,41 @@ __author__ = 'Jack'
 import sqlite3
 import os.path
 
-mypath = './'
-dbname = 'myfirstsqlite.db'
 
-if not os.path.exists(mypath + dbname):
-    conn = sqlite3.connect(dbname)
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE fruit
-                (name TEXT, calories INT, price NUMERIC)
-                 """)
-    print('db created')
+def Main():
+    mypath = './'
+    dbname = 'myfirstsqlite.db'
+    try:
+        if not os.path.exists(mypath + dbname):
+            conn = sqlite3.connect(dbname)
+            cursor = conn.cursor()
+            cursor.execute("""CREATE TABLE fruit
+                        (name TEXT, calories INT, price NUMERIC)
+                         """)
+            print('db created')
 
-conn = sqlite3.connect(dbname)
-cursor = conn.cursor()
+        conn = sqlite3.connect(dbname)
+        cursor = conn.cursor()
 
-fruit = [('banana', '30', '0.5'),
-         ('pinapple', '12', '100'),
-         ('kiwi', '10', '1.1')]
-cursor.executemany('INSERT INTO fruit VALUES(?,?,?)', fruit)
-conn.commit()
+        fruit = [('banana', '30', '0.5'),
+                 ('pinapple', '12', '100'),
+                 ('kiwi', '10', '1.1')]
+        cursor.executemany('INSERT INTO fruit VALUES(?,?,?)', fruit)
+        conn.commit()
 
-sql = "SELECT * FROM fruit WHERE name=?"
-cursor.execute(sql, ["kiwi"])
-print(cursor.fetchall())
+        sql = "SELECT * FROM fruit WHERE name=?"
+        cursor.execute(sql, ["kiwi"])
+        print(cursor.fetchall())
+
+    except sqlite3.Error:
+        if conn:
+            print("Error! Rolling back")
+            conn.rollback()
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == "__main__":
+    Main()
+
---
+++ 3.advance/udpServer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    print('Server started')
+    while True:
+        data, addr = s.recvfrom(1024)
+        data = data.decode('utf-8')
+        print("Message from: " + str(addr))
+        print("From connected user: " + data)
+        data = data.upper()
+        print("Sending: " + data)
+        s.sendto(data.encode('utf-8'), addr)
+
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
